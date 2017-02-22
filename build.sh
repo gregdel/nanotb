d0f5b9644e73f74cd54576dfcf2e28cc6ddf275a
Adrien Gallouët <adrien@gallouet.fr>
2017-02-22 14:29:26 +0000

    Code cleanup

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-OTB_TAG=`git describe --tags --match='v[0-9].*' 2>/dev/null`
+set -e
 
-export OTB_VERSION=${OTB_TAG#v}
-export OTB_BRANCH=master
+OTB_VERSION=0.8
+OTB_BRANCH=master
 
 [ -d openwrt ] || \
 	git clone --depth=1 https://github.com/openwrt/openwrt --branch ${OTB_BRANCH}
@@ -17,21 +17,20 @@ cp config openwrt/.config
 
 [ -f setup.sh ] && sh setup.sh
 
-cd openwrt
+(
+	cd openwrt
 
-echo ${OTB_VERSION} > version
+	echo "${OTB_VERSION}" > version
 
-cat > feeds.conf <<EOF
-src-git packages https://github.com/openwrt/packages.git
-EOF
+	cat > feeds.conf <<-EOF
+	#src-git packages https://github.com/openwrt/packages.git
+	EOF
 
-./scripts/feeds update -a
-./scripts/feeds install -p packages sqm-scripts sqm-scripts-extra
+	#./scripts/feeds update -a
+	#./scripts/feeds install -p packages sqm-scripts sqm-scripts-extra
 
-make defconfig
-make clean
-make -j$(nproc)
-
-cd ..
+	make defconfig
+	make -j"$(nproc)"
+)
 
 [ -d ~/dl ] && rsync -avh openwrt/dl/ ~/dl/
