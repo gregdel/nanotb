5035735484568bcb93b82455bf476a519781abc2
Adrien Gallouët <adrien@gallouet.fr>
2017-03-23 15:36:52 +0000

    Update
    * Use lede 17.01
    * Remove otb-action-update
    * Add otb-action-upgrade

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 
 OTB_VERSION=0.8
-OTB_BRANCH=master
+OTB_BRANCH=lede-17.01
+OTB_REPO=http://$(curl -sS ipaddr.ovh):8000
 
 [ -d build ] || \
 	git clone --depth=1 https://git.lede-project.org/source.git --branch ${OTB_BRANCH} build
@@ -18,6 +19,7 @@ CONFIG_DOWNLOAD_FOLDER="$(pwd)/dl"
 CONFIG_IMAGEOPT=y
 CONFIG_VERSIONOPT=y
 CONFIG_VERSION_DIST="nanOTB"
+CONFIG_VERSION_REPO="${OTB_REPO}"
 CONFIG_PACKAGE_remote=y
 CONFIG_PACKAGE_ca-bundle=y
 CONFIG_PACKAGE_ca-certificates=y
@@ -27,7 +29,7 @@ EOF
 echo "${OTB_VERSION}" > build/version
 touch build/feeds.conf
 
-[ -f setup.sh ] && sh setup.sh
+[ -f key-build ] && cp -f key-build* build
 
 (
 	cd build
