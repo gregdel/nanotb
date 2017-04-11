acf4e1e40e7742b2e50b56d1209968aec8512a2f
Adrien Gallouët <adrien@gallouet.fr>
2017-04-11 06:48:14 +0000

    Update build process and add pi3

--- build.sh
+++ build.sh
@@ -2,45 +2,60 @@
 
 set -e
 
-OTB_VERSION=0.8
-OTB_BRANCH=lede-17.01
-OTB_REPO=http://$(curl -sS ipaddr.ovh):8000
+OTB_DIST=${OTB_DIST:-nanotb}
+OTB_REPO=${OTB_REPO:-http://$(curl -sS ipaddr.ovh):8000}
 
-[ -d build ] || \
-	git clone --depth=1 https://git.lede-project.org/source.git --branch ${OTB_BRANCH} build
+OTB_BRANCH=${OTB_BRANCH:-x86_uefi}
+OTB_SOURCE=https://github.com/sduponch/source.git
 
-rsync -avh files/ build/files/
-rsync -avh package/ build/package/otb/
+[ -d source ] || \
+	git clone --depth=1 "${OTB_SOURCE}" --branch "${OTB_BRANCH}" source
 
-cat > build/.config <<EOF
-CONFIG_DEVEL=y
-CONFIG_DOWNLOAD_FOLDER="$(pwd)/dl"
+cp -rf files source
+cp -rf package source/package/otb
+
+[ -f key-build ] && cp -f key-build* source
+
+rm -rf source/.config
+[ -f config ] && cp -f config source/.config
+
+cd source
+
+cat >> .config <<EOF
 CONFIG_IMAGEOPT=y
 CONFIG_VERSIONOPT=y
-CONFIG_VERSION_DIST="nanOTB"
+CONFIG_VERSION_DIST="${OTB_DIST}"
 CONFIG_VERSION_REPO="${OTB_REPO}"
-CONFIG_PACKAGE_otb-meta=y
-CONFIG_PACKAGE_iperf3=m
-CONFIG_PACKAGE_strace=m
-CONFIG_PACKAGE_tc=m
-CONFIG_PACKAGE_tcpdump-mini=m
+CONFIG_PACKAGE_${OTB_DIST}=y
 EOF
 
-[ -f config ] && cat config >> build/.config
+case "$1" in
+	pi3)
+		cat >> .config <<-EOF
+		CONFIG_TARGET_brcm2708=y
+		CONFIG_TARGET_brcm2708_bcm2710=y
+		CONFIG_TARGET_brcm2708_bcm2710_DEVICE_rpi-3=y
+		EOF
+		;;
+	nuc)
+		cat >> .config <<-EOF
+		CONFIG_TARGET_x86=y
+		CONFIG_TARGET_x86_64=y
+		CONFIG_TARGET_x86_64_Generic=y
+		EOF
+		;;
+esac
 
-echo "${OTB_VERSION}" > build/version
-touch build/feeds.conf
+shift
 
-[ -f key-build ] && cp -f key-build* build
+OTB_VERSION=$(git describe --always)
+echo "${OTB_VERSION}" > version
 
-(
-	cd build
+make defconfig
+make "$@"
 
-	make defconfig
-	make clean
-	make V=w -j"$(nproc)"
+find bin -name "${OTB_DIST}*${OTB_VERSION}*" -and -not -name "*.sig" \
+	-exec ./staging_dir/host/bin/usign -S -m {} -s key-build \;
 
-	find bin -name '*.img*' -exec ./staging_dir/host/bin/usign -S -m {} -s key-build \;
-	find bin -name '*combined*.img.gz'     -execdir ln -sf {} latest.img.gz     \;
-	find bin -name '*combined*.img.gz.sig' -execdir ln -sf {} latest.img.gz.sig \;
-)
+find bin -name '*combined*.img.gz'     -execdir ln -sf {} latest.img.gz     \;
+find bin -name '*combined*.img.gz.sig' -execdir ln -sf {} latest.img.gz.sig \;
