f809c86639ac85f9fa35a9ae38d0a3cd1315d32c
Adrien Gallouët <adrien@gallouet.fr>
2017-05-30 10:46:49 +0200

    Get last code from overthebox

--- build.sh
+++ build.sh
@@ -4,35 +4,36 @@ set -e
 
 OTB_DIST=${OTB_DIST:-nanotb}
 OTB_REPO=${OTB_REPO:-http://$(curl -sS ipaddr.ovh):8000}
-
-OTB_BRANCH=${OTB_BRANCH:-lede-17.01}
 OTB_SOURCE=https://github.com/ovh/overthebox-lede
-
+OTB_NUMBER=17.05.30
 OTB_VERSION=$(git rev-parse --short HEAD)
 
 [ -d source ] || \
-	git clone --depth=1 "${OTB_SOURCE}" --branch "${OTB_BRANCH}" source
+	git clone --depth 1 "${OTB_SOURCE}" --branch "otb-${OTB_NUMBER}" source
 
 rsync -avh custom/ source/
+
+cat > source/feeds.conf <<EOF
+src-git packages https://git.lede-project.org/feed/packages.git;lede-17.01
+src-link feed $(pwd)/feed
+EOF
 
 cd source
 
 echo "${OTB_VERSION}" > version
+
+./scripts/feeds update -a
+./scripts/feeds install -a -d m -f -p feed
 
 cat >> .config <<EOF
 CONFIG_IMAGEOPT=y
 CONFIG_VERSIONOPT=y
 CONFIG_VERSION_DIST="${OTB_DIST}"
 CONFIG_VERSION_REPO="${OTB_REPO}"
+CONFIG_VERSION_NUMBER="${OTB_NUMBER}"
 CONFIG_PACKAGE_${OTB_DIST}=y
 EOF
 
 make defconfig
 make clean
 make "$@"
-
-find bin -name "${OTB_DIST}*${OTB_VERSION}*" -and -not -name "*.sig" \
-	-exec ./staging_dir/host/bin/usign -S -m {} -s key-build \;
-
-find bin -name '*combined*.img.gz'     -execdir ln -sf {} latest.img.gz     \;
-find bin -name '*combined*.img.gz.sig' -execdir ln -sf {} latest.img.gz.sig \;
