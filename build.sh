9182453b5c5445a18ec585894083dac3c91d25f1
Adrien Gallouët <adrien@gallouet.fr>
2017-06-23 08:57:13 +0200

    Update build

--- build.sh
+++ build.sh
@@ -2,42 +2,43 @@
 
 set -e
 
-OTB_DIST=${OTB_DIST:-nanotb}
-OTB_REPO=${OTB_REPO:-http://$(curl -sS ipaddr.ovh):8000}
-OTB_SOURCE=https://github.com/ovh/overthebox-lede
-OTB_NUMBER=17.06.18
-OTB_VERSION=$(git rev-parse --short HEAD)
+OTB_NUMBER=$(git describe --tag --always)
 
-[ -d source ] || \
-	git clone --depth 1 "${OTB_SOURCE}" --branch "otb-${OTB_NUMBER}" source
+OTB_SRC=${OTB_SRC:-17.06.18}
+OTB_REPO=${OTB_REPO:-http://$(curl -sS ipaddr.ovh):8000}
+OTB_DIST=nanotb
+
+git clone https://github.com/ovh/overthebox-lede source || true
+git -C source fetch --all
+git -C source checkout "origin/otb-$OTB_SRC" -B "otb-$OTB_SRC"
+
+echo "$OTB_SRC" > source/version
 
 rsync -avh custom/ source/
 
 cat > source/feeds.conf <<EOF
 src-git packages https://git.lede-project.org/feed/packages.git;lede-17.01
-src-link feed $(pwd)/feed
+src-git luci https://github.com/openwrt/luci.git;for-15.05
+src-link feed $(readlink -f feed)
+EOF
+
+cat >> source/.config <<EOF
+CONFIG_IMAGEOPT=y
+CONFIG_VERSIONOPT=y
+CONFIG_VERSION_DIST="$OTB_DIST"
+CONFIG_VERSION_REPO="$OTB_REPO"
+CONFIG_VERSION_NUMBER="$OTB_NUMBER"
+CONFIG_PACKAGE_$OTB_DIST=y
 EOF
 
 cd source
 
-echo "${OTB_VERSION}" > version
+echo "Building $(cat version)"
 
 cp .config .config.keep
-./scripts/feeds update -a
-./scripts/feeds install -a -d y -f -p feed
+scripts/feeds update -a
+scripts/feeds install -a -d y -f -p feed
 cp .config.keep .config
 
-cat >> .config <<EOF
-CONFIG_IMAGEOPT=y
-CONFIG_VERSIONOPT=y
-CONFIG_VERSION_DIST="${OTB_DIST}"
-CONFIG_VERSION_REPO="${OTB_REPO}"
-CONFIG_VERSION_NUMBER="${OTB_NUMBER}"
-CONFIG_PACKAGE_${OTB_DIST}=y
-EOF
-
 make defconfig
-make clean
-make "$@" || exit
-
-find bin -name '*.img*' -exec ./staging_dir/host/bin/usign -S -m {} -s key-build \;
+make "$@"
